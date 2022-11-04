2d5ac65200908f5911cfa62dc470ebf78391be09
Tom <tomlonergan91@gmail.com>
2022-11-04 16:53:57 +0000

    Catch exceptions and allow non-implemented tests

--- tester.py
+++ tester.py
@@ -7,6 +7,7 @@ class colours:
     PASS = '\033[92m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
+    WARNING = '\033[93m'
 
 
 total = 0
@@ -25,17 +26,37 @@ def runTests(*tests):
     printResult()
 
 
-def assertEqual(name, correct_result, function=lambda x: x, *args, **kwargs):
+def test(name, correct_result="NOT IMPLEMENTED", function=lambda x: x, *args, **kwargs):
     global total, passed
     total = total + 1
-    testname = "Test " + str(total) + ": " + str(name)
+
+    if (correct_result == "NOT IMPLEMENTED"):
+        testname = "Test " + str(total) + ": " + name
+        print(f"{testname:<50}", sep="", end=" ")
+        print(f'{(colours.WARNING + "NOT IMPLEMENTED" + colours.ENDC):>10}')
+        return
+
+    testname = "Test " + str(total) + ": " + name
     print(f"{testname:<50}", sep="", end=" ")
-    actual_result = function(*args, **kwargs)
+    test_start_time = time()
+    try:
+        actual_result = function(*args, **kwargs)
+    except Exception as e:
+        actual_result = e
+
+    test_run_time = round(time() - test_start_time, 4)
     if (correct_result == actual_result):
-        print(f'{(colours.PASS + "Passed" + colours.ENDC):>10}')
+        print(f'{(colours.PASS + "Passed" + colours.ENDC):>10}', end=" ")
+        print(test_run_time, "s")
         passed = passed + 1
+    elif (type(correct_result) == Exception):
+        print(f'{(colours.FAIL + "Failed" + colours.ENDC):>10}', end=" ")
+        print(test_run_time, "s")
+        print(actual_result)
+        print("")
     else:
-        print(f'{(colours.FAIL + "Failed" + colours.ENDC):>10}')
+        print(f'{(colours.FAIL + "Failed" + colours.ENDC):>10}', end=" ")
+        print(test_run_time, "s")
         print("Expected\n", correct_result, "\ngot\n", actual_result)
 
 
