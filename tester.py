802f6a10c522b02391e2ed9080f5b53912b3a5e3
Tom <tomlonergan91@gmail.com>
2022-11-04 12:36:55 +0000

    Names of tests + formatting

--- tester.py
+++ tester.py
@@ -1,26 +1,35 @@
 from sys import argv
+from time import time
+
 
 class colours:
+    HEADER = '\033[95m'
     PASS = '\033[92m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
+
 total = 0
 passed = 0
+start_time = 0
+
 
 def run(*tests):
-    global total, passed
+    global total, passed, start_time
     total = 0
-    passed = 0  
+    passed = 0
+    start_time = time()
     for test in tests:
+        print(colours.HEADER + test.__name__ + colours.ENDC)
         test()
     printResult()
 
-def assertEqual(name, correct_result, function = lambda x:x, *args):
+
+def assertEqual(name, correct_result, function=lambda x: x, *args):
     global total, passed
     total = total + 1
     testname = "Test " + str(total) + ": " + str(name)
-    print(f"{testname:<50}", sep="",end=" ")
+    print(f"{testname:<50}", sep="", end=" ")
     actual_result = function(*args)
     if (correct_result == actual_result):
         print(f'{(colours.PASS + "Passed" + colours.ENDC):>10}')
@@ -29,11 +38,15 @@ def assertEqual(name, correct_result, function = lambda x:x, *args):
         print(colours.FAIL + "Failed" + colours.ENDC)
         print("Expected", correct_result, "got", actual_result)
 
+
 def printResult():
     global total, passed
     result = round(passed/total*100)
 
     if result == 100:
-        print(f'{(colours.PASS + str(passed) + " out of " + str(total) + " total tests"):<50}' + f'{(str(result) + "%" + colours.ENDC):>14}')
+        status = colours.PASS
     else:
-        print(f'{(colours.FAIL + str(passed) + " out of " + str(total) + " total tests"):<50}' + f'{(str(result) + "%" + colours.ENDC):>14}')
+        status = colours.FAIL
+
+    print(f'{(status + str(passed) + " out of " + str(total) + " tests passed in " + str(round((time()-start_time), 4)) + " seconds"):<50}' +
+          f'{(str(result) + "%" + colours.ENDC):>14}')
