40e7084fec67816ffaadc607adb55d15ae0eb7ea
Tom <tomlonergan91@gmail.com>
2022-11-05 13:30:06 +0000

    Accept args for testgroups that should return error outputs

--- tester.py
+++ tester.py
@@ -12,26 +12,50 @@ class colours:
 
 total = 0
 passed = 0
-start_time = 0
-report_failure_reason = False
+report_all_failures = False
+report_failure_module = []
+report_test_failure = False
 
 
 def runTests(*tests):
-    global total, passed, start_time, report_failure_reason
+    global total, passed, report_all_failures, report_failure_module, report_test_failure
     total = 0
     passed = 0
     if len(argv) > 1:
-        if argv[1] == str(1):
-            report_failure_reason = True
+        if type(argv[1]) == str:
+            if argv[1] == "all":
+                report_all_failures = True
+            else:
+                try:
+                    report_failure_module = [int(x) for x in argv[1]
+                                             .replace("[", "")
+                                             .replace("]", "")
+                                             .split(',')]
+                except:
+                    print("Unrecognised arguement")
+                    print("Accepted arguements:")
+                    print("all - output all failure strings")
+                    print("[x,y,z] - outputs failures for testgroup x,y,z")
+                    quit()
+
     start_time = time()
+    i = 0
     for test in tests:
-        print(colours.HEADER + test.__name__ + colours.ENDC)
+        if report_all_failures:
+            report_test_failure = True
+        elif (report_failure_module.__contains__(i)):
+            report_test_failure = True
+        else:
+            report_test_failure = False
+
+        print(colours.HEADER + str(i) + ". " + test.__name__ + colours.ENDC)
         test()
-    printResult()
+        i += 1
+    printResult(start_time)
 
 
 def test(name, correct_result="NOT IMPLEMENTED", function=lambda x: x, *args, **kwargs):
-    global total, passed, report_failure_reason
+    global total, passed, report_all_failures
     total = total + 1
 
     if (correct_result == "NOT IMPLEMENTED"):
@@ -56,17 +80,17 @@ def test(name, correct_result="NOT IMPLEMENTED", function=lambda x: x, *args, **
     elif (type(correct_result) == Exception):
         print(f'{(colours.FAIL + "FAILED" + colours.ENDC):>10}', end=" ")
         print(test_run_time, "s")
-        if report_failure_reason:
+        if report_test_failure:
             print(actual_result)
         print("")
     else:
         print(f'{(colours.FAIL + "FAILED" + colours.ENDC):>10}', end=" ")
         print(test_run_time, "s")
-        if report_failure_reason:
+        if report_test_failure:
             print("Expected\n", correct_result, "\ngot\n", actual_result)
 
 
-def printResult():
+def printResult(start_time):
     global total, passed
     result = round(passed/total*100)
 
@@ -75,5 +99,5 @@ def printResult():
     else:
         status = colours.FAIL
 
-    print(f'{(status + str(passed) + " out of " + str(total) + " tests passed in " + str(round((time()-start_time), 4)) + " seconds"):<50}' +
+    print(f'{(status + str(passed) + " out of " + str(total) + " tests passed in " + str(round((time() - start_time), 4)) + " seconds"):<50}' +
           f'{(str(result) + "%" + colours.ENDC):>14}')
